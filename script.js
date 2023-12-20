654d21a70110ab8378b721f7707bbc989ae1c8f5
Sachin <86548111+sachusachin@users.noreply.github.com>
2023-12-20 13:21:02 +0000

    intro added

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const mytext = new SplitType("#intro-text")
+async function introPlay(){
+    document.querySelector('#intro-text').style.opacity='1'
+    await gsap.to(".char", {y:0, opacity:1 ,stagger:0.05, delay:.5,duration:1});
+
+    homeLoad()
+}
+
+window.onload = introPlay()
+
 let playBtn = document.querySelector('#play-reel')
 let homePage = document.querySelector('#home')
 
@@ -37,6 +47,32 @@ function animateMenus() {
     tl.play(0);
     gsap.fromTo("#sampImg", { scale: 0 }, { scale:1, duration: 1 , delay:.5 });
     t2.play(0);
+}
+
+
+const title = 'Shirataii Carbons'
+
+
+
+
+for(i of title){
+    t = document.createElement('span')
+    t.textContent=i
+    t.classList.add('tLetter')
+    document.querySelector('#title-text').appendChild(t)
+}
+
+let letter = document.querySelectorAll('.tLetter')
+var l1 = new TimelineMax({paused:true});
+l1.staggerFromTo(letter, 1, {opacity:0,y:20}, {opacity:1,y:-10}, {opacity:1,y:0}, 0.5, "-=1");
+
+
+function homeLoad(){
+    document.querySelector('#intro-box').style.display='none'
+    gsap.fromTo("#myVideo", { opacity: 0 }, { opacity: 1, duration: 1, delay: 1 });
+    gsap.fromTo("#menu-btn", { opacity: 0, x: -100 }, { opacity: 1, x: 0, duration: 1, delay: 1.5 });
+    gsap.fromTo("#logo-img", { opacity: 0, x: 100 }, { opacity: 1, x: 0, duration: 1, delay: 1.5 });
+    l1.play(0);
 }
 
 
